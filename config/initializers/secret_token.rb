1379219f6d453ff41d5fb265534cbad1f768544e
vagrant <paul.val1@yahoo.ca>
2014-06-02 23:52:06 +0000

    generating movies controller and views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wk5d1RottenMangoes::Application.config.secret_key_base = '68f0a87ad5ac422d2998e2beb9a14a01bcddb841eff089c5485e654b9e9c857da5f0362f74a8718cb44c24f3d618cb8bdc8f76a4b5e9453060034a4abe85d65b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wk5d1RottenMangoes::Application.config.session_store :cookie_store, key: '_wk5d1_rotten_mangoes_session'
