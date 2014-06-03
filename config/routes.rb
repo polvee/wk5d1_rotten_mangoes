32869b8ec8aea1c9b29be87f6ba3106c7d2d968e
vagrant <paul.val1@yahoo.ca>
2014-06-03 02:32:20 +0000

    routing root to movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Wk5d1RottenMangoes::Application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-
+  root to: 'movies#index'
 
 
   # get "movies/index"
