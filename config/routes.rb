4c9de3843d3411aba4e9908f14ed2683c74c5395
vagrant <paul.val1@yahoo.ca>
2014-06-03 02:16:21 +0000

    14. using a before_filter in the reviews_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Wk5d1RottenMangoes::Application.routes.draw do
   
 
+  get "reviews/new"
+  get "reviews/create"
   get "sessions/new"
   get "sessions/create"
   get "users/new"
   get "users/create"
-  resources :movies
+  
+    resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
